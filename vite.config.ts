c5020b8bac7db0237558d4d67fa770ebed7b4d08
Albert Sanchez <albert.san.iza@gmail.com>
2025-12-07 21:08:07 -0800

    feat: update Vite and Babel plugins for improved React support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
     base: '/japanese-characters',
-    plugins: [react(), tailwindcss()],
+    plugins: [react({ babel: { plugins: [['babel-plugin-react-compiler']] } }), tailwindcss()],
     resolve: {
         alias: {
+            '@/convex': path.resolve(__dirname, './convex'),
             '@': path.resolve(__dirname, './src')
         }
     }

0b73f13d000f6e5ba35a8a30ed8482cf9e428897
Albert Sanchez <albert.san.iza@gmail.com>
2025-12-07 21:37:57 -0800

    feat: wrap App component with ClerkProvider and ConvexProviderWithClerk for authentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
+import { ClerkProvider, useAuth } from '@clerk/clerk-react'
+import { ConvexReactClient } from 'convex/react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from '@/App'
 import '@/index.css'
+import { VITE_CLERK_PUBLISHABLE_KEY, VITE_CLERK_SIGN_IN_FORCE_REDIRECT_URL, VITE_CLERK_SIGN_UP_FORCE_REDIRECT_URL } from '@/lib/utils'
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <ClerkProvider
+            publishableKey={VITE_CLERK_PUBLISHABLE_KEY}
+            signInForceRedirectUrl={VITE_CLERK_SIGN_IN_FORCE_REDIRECT_URL}
+            signUpForceRedirectUrl={VITE_CLERK_SIGN_UP_FORCE_REDIRECT_URL}
+        >
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <App />
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
     </StrictMode>
 )
